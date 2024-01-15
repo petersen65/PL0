6e269faa1e5e3b22ec4a3864fa40e8c593633498
Michael Petersen <mikepet@me.com>
2024-01-15 13:27:15 +0100

    Much improved compile/run/test workflow

--- main.go
+++ main.go
@@ -17,29 +17,67 @@ func main() {
 
 	switch {
 	case len(os.Args) > 3 && os.Args[1] == "-c" && len(os.Args[2]) > 0 && len(os.Args[3]) > 0:
-		if err := compiler.CompileFile(os.Args[2], os.Args[3], os.Stdout); err != nil {
-			fmt.Println("Summary:", err)
-		} else {
-			fmt.Println("Compilation successful")
-		}
+		compile(os.Args[2], os.Args[3])
 
 	case len(os.Args) > 2 && os.Args[1] == "-r" && len(os.Args[2]) > 0:
-		if err := compiler.RunFile(os.Args[2], os.Stdout); err != nil {
-			fmt.Println("Summary:", err)
-		} else {
-			fmt.Println("Run successful")
-		}
+		run(os.Args[2])
 
 	case len(os.Args) > 2 && os.Args[1] == "-p" && len(os.Args[2]) > 0:
-		if err := compiler.PrintFile(os.Args[2], os.Stdout); err != nil {
-			fmt.Println("Summary:", err)
-		} else {
-			fmt.Println("Print successful")
+		print(os.Args[2])
+
+	case len(os.Args) > 3 && os.Args[1] == "-cr" && len(os.Args[2]) > 0 && len(os.Args[3]) > 0:
+		if compile(os.Args[2], os.Args[3]) == nil {
+			run(os.Args[3])
+		}
+
+	case len(os.Args) > 3 && os.Args[1] == "-cp" && len(os.Args[2]) > 0 && len(os.Args[3]) > 0:
+		if compile(os.Args[2], os.Args[3]) == nil {
+			print(os.Args[3])
 		}
 
 	default:
-		fmt.Println("Usage: pl0 -c <source file> <target file>")
-		fmt.Println("       pl0 -r <target file>")
-		fmt.Println("       pl0 -p <target file>")
+		usage()
 	}
+}
+
+func compile(pl0, il0 string) error {
+	err := compiler.CompileFile(pl0, il0, os.Stdout)
+
+	if err != nil {
+		fmt.Println("Summary:", err)
+	} else {
+		fmt.Println("Compilation successful")
+	}
+
+	return err
+}
+
+func run(il0 string) error {
+	err := compiler.RunFile(il0, os.Stdout)
+
+	if err != nil {
+		fmt.Println("Summary:", err)
+	} else {
+		fmt.Println("Run successful")
+	}
+
+	return err
+}
+
+func print(il0 string) error {
+	err := compiler.PrintFile(il0, os.Stdout)
+
+	if err != nil {
+		fmt.Println("Summary:", err)
+	} else {
+		fmt.Println("Print successful")
+	}
+
+	return err
+}
+
+func usage() {
+	fmt.Println("Usage: pl0 -c[r|p] <source file> <target file>")
+	fmt.Println("       pl0 -r <target file>")
+	fmt.Println("       pl0 -p <target file>")
 }
