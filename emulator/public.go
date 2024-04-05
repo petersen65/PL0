d8cad7c549de499ee1facb64c324698b4028f938
Michael Petersen <mikepet@me.com>
2024-04-06 01:22:51 +0200

    failure while committing

--- emulator/public.go
+++ emulator/public.go
@@ -8,10 +8,10 @@ import cod "github.com/petersen65/PL0/v2/code"
 
 // Run a binary AS/C target and return an error if the target fails to execute.
 func Run(raw []byte) error {
-	return newMachine().runProgram(raw)
+	return newMachine().runRaw(raw)
 }
 
 // Load an IL/C module and return an error if the module fails to JIT compile and execute as AS/C.
 func RunModule(module cod.Module) error {
-	return newMachine().runProcess(module)
+	return newMachine().runModule(module)
 }
