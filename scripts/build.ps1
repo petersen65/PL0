9ee1874623f090e69e942f70892ecdc91139a62e
Michael Petersen <mikepet@me.com>
2025-01-02 17:49:35 +0100

    Copyright headers in build scripts

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,2 +1,5 @@
+# Copyright 2024-2025 Michael Petersen. All rights reserved.
+# Use of this source code is governed by an Apache license that can be found in the LICENSE file.
+
 Set-Variable -Name 'LinkCommand' -Value "-X main.CommitHash=$(git rev-parse --short HEAD)"
 go build -v -a -o bin/pl0.exe -ldflags $LinkCommand
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Copyright 2024-2025 Michael Petersen. All rights reserved.
+# Use of this source code is governed by an Apache license that can be found in the LICENSE file.
 
 link_command="-X main.CommitHash=$(git rev-parse --short HEAD)"
 go build -v -a -o bin/pl0 -ldflags "$link_command"
